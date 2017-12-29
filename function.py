e42b3b9e6189f5674159fa64552cd93c21dacb52
BANERJEE786 <priyambanerjee08@gmail.com>
2017-12-29 12:12:10 -0500

    final changes

--- function.py
+++ function.py
@@ -3,8 +3,9 @@ import os
 import sys
 import random
 
+#Function to add two numbers
 def numAdd(num1,num2):
   addedNum = num1 + num2
   return addedNum
  
-print(numAdd(10,25))
+print(numAdd(10,25)) #Printing the two numbers
