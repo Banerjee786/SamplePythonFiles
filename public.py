08fd4fc1f415fd801db06bf9b32caee7899bf991
BANERJEE786 <priyambanerjee08@gmail.com>
2018-01-02 22:29:36 -0500

    public.py file modificaion

--- public.py
+++ public.py
@@ -5,7 +5,7 @@ import sys
 class Animal:
     '''
     Constructor below :
-    '''
+
     def __init__(self):
         self.name = None
         self.height = None
@@ -16,18 +16,37 @@ class Animal:
         self.name = name
         self.height = height
         self.weight = weight
-    '''
+
     def Name(self,animalName):
         self.name = animalName
     def Height(self,animalHeight):
         self.height = animalHeight
     def Weight(self,animalWeight):
         self.weight = animalWeight
+
     def displayInfo(self):
         return "{} is {} cm tall and weighs {} kgs".format(self.name,self.height,self.weight)
-animalObj = Animal()
-# animalObj = Animal('Cheetah','120','145') In case constructor contains arguments
-animalObj.name = 'Leopard'
-animalObj.height = '125'
-animalObj.weight = '85'
+#animalObj = Animal()
+animalObj = Animal('Cheetah','120','145') #In case constructor contains arguments
+#animalObj.name = 'Leopard'
+#animalObj.height = '125'
+#animalObj.weight = '85'
 print(animalObj.displayInfo())
+
+
+class Dog(Animal):
+    owner = ""
+    def __init__(self,name,height,weight,owner):
+        '''
+        self.name = name
+        self.height = height
+        self.weight = weight
+        '''
+        self.owner = owner
+        super(Dog,self).__init__(name,height,weight)
+    def set_owner(self,owner):
+        self.owner = owner
+    def toString(self):
+        return "{} is {} cm tall and weighs {} kgs. Owner is {}".format(self.name,self.height,self.weight,self.owner)
+dogObj = Dog('Tarzan',122,20,'Jason Cummins')
+print(dogObj.toString())
